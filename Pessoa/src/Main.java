bff19f06cf786b7f3e1c68fa28d11f05054855d5
Fantiko <86378890+Fantiko@users.noreply.github.com>
2022-11-06 19:52:21 -0300

    ADD "atv livro"

---
+++ Pessoa/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+
+        p[0]= new Pessoa("Pedro",22, "M");
+        p[1]= new Pessoa("Maria", 25, "F");
+
+        l[0]= new Livro("Aprendendo java", "Jose da Silva", 300, p[0]);
+        l[1]= new Livro("POO in a nutshell", "Predo Paulo", 500, p[1]);
+        l[2]= new Livro("Java avançado", "Guilherme Bastaviel", 800, p[0]);
+
+        System.out.println(l[1].detalhes());
+
+
+    }
+}
