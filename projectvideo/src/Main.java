0e27e0c8b715e696dcdd05e9fe94cd2ab799d1e7
Fantiko <86378890+Fantiko@users.noreply.github.com>
2022-11-12 19:42:07 -0300

    finalizacao

--- projectvideo/src/Main.java
+++ projectvideo/src/Main.java
@@ -5,6 +5,22 @@ public class Main {
         v[1] = new Video("Chainsawman Full OP - 1 hour");
         v[2] = new Video("Rock Lee vs Gaara - Link Park");
 
-        System.out.println(v[2].toString());
+        //System.out.println(v[2].toString());
+
+        Aluno[] g = new Aluno[2];
+        g[0] = new Aluno("fernando",33,"m",0,"aluno");
+        g[1] = new Aluno("andre",43,"m",100,"andre");
+
+        Visualizacao[] vis = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0],v[1]);
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(g[0],v[1]);
+        vis[1].avaliar(87.0f);
+        System.out.println(vis[1].toString());
+
+
+
     }
 }
